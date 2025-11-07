97e0feabe66ea5428ab18292d02fc185fb936ee8
wassimhajjijunior <wassim.hajji@supcom.tn>
2025-11-07 23:20:59 +0100

    try fix the deploy prob

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ tailwindcss(),
-    react()],
-})
+  plugins: [react()],
+  base: "./", // <-- important for deployment
+});
