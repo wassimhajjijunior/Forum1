c5f76a9dce5d01dc5aed07e239a2946a30de5868
wassimhajjijunior <wassim.hajji@supcom.tn>
2025-11-08 21:41:07 +0100

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/', // ✅ important for correct asset paths on Render
-  build: {
-    outDir: 'dist', // default build output folder (Render expects this)
-    assetsDir: 'assets', // keep assets organized
-  },
-  server: {
-    port: 5173, // default dev port, optional
-    open: true, // auto open in browser during dev
-  },
+  plugins: [ tailwindcss(),
+    react()],
+    base: "/",
+
 })
